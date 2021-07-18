a8f9d214deff70bdd6263724cd2b3503f1d13db6
Ahnaf Sayed <ahnafsayedtashri@gmail.com>
2021-07-18 16:55:30 +0600

    MVC Complete!

---
+++ Final Lab Task 1 MVC/Views/Footer.php
@@ -0,0 +1,6 @@
+<html>
+    <head></head>
+	<footer>
+	    <h4 align="center">Final Lab Task 1 MVC</h4>
+	</footer>
+</html>
---
+++ Final Lab Task 1 MVC/Views/MainHeader.php
@@ -0,0 +1,8 @@
+<html>
+    <head></head>
+	<body>
+	    <div align = "center">
+		    <h3>Final Lab Task 1</h3>
+		</div>
+	</body>
+</html>
